f77ada6e5f449296566db19d171593e4021459e5
John HC <69842878+Purgatos81@users.noreply.github.com>
2021-02-19 13:28:17 +0100

    Form next

--- js/script2.js
+++ js/script2.js
@@ -1,6 +1,7 @@
 //details js
 
 const ResultContainer = document.querySelector(".thisBrewerie");
+const DetailTitle = document.querySelector(".detailsTitle");
 console.log(ResultContainer);
 
 const QueryString = document.location.search;
@@ -16,7 +17,28 @@ const ThisURL = "https://api.openbrewerydb.org/breweries/" + id;
 console.log(ThisURL);
 
 async function fetchBrewerie() {
+    try {
+        const resp = await fetch(ThisURL);
+        const BrewerieDetails = await resp.json();
+        console.log(BrewerieDetails);
 
+        DetailTitle.innerHTML = `${BrewerieDetails.name}`
+
+        ResultContainer.innerHTML = `<h1>${BrewerieDetails.name}</h1>
+        <p>${BrewerieDetails.brewery_type}</p>
+        <p>${BrewerieDetails.country}</p>
+        <p>${BrewerieDetails.state}</p>
+        <p>${BrewerieDetails.city}</p>
+        <p>${BrewerieDetails.street}</p>
+        <p>${BrewerieDetails.phone}</p>
+        <p>${BrewerieDetails.website_url}</p>
+        `
+
+    }
+    catch(error) {
+        console.log(error);
+        ResultContainer.innerHTML = message("error", error);
+    }
 }
 
 fetchBrewerie()
